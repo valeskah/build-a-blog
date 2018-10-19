b860ce98796ab4f037cbe4614256415ff9687143
Valeska Halamicek <vjhalamicek@gmail.com>
2018-10-18 21:44:38 -0500

    made and set up database configuration

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:lc101@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class Blog_post(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    blog_title = db.Column(db.String(120))
+    blog_body = db.Column(db.String(120))
+
+    def __init__(self, blog_title, blog_body):
+        self.blog_title = blog_title
+        self.blog_body = blog_body
+
+
 
 posts = []
 
@@ -14,4 +30,5 @@ def index():
 
     return render_template('blog_posts.html', title='Build-a-Blog', posts=posts)
 
-app.run()
+if __name__ == '__main__':
+    app.run()
