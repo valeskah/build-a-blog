6e4beca1baffcaf3080fda50ca70a753eaa5b385
Valeska Halamicek <vjhalamicek@gmail.com>
2018-10-25 11:28:18 -0500

    flash error messages

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,6 +6,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:lc101@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'chamberofsecrets'
 
 class Blog_post(db.Model):
 
@@ -28,12 +29,21 @@ def new_post():
 def index():
 
     if request.method == 'POST':
-       blog_title = request.form['blog_title']
-       blog_body = request.form['blog_body']
-       new_blog_post = Blog_post(blog_title, blog_body)
-       db.session.add(new_blog_post)
-       db.session.commit()
+        blog_title = request.form['blog_title']
+        blog_body = request.form['blog_body']
 
+        if blog_title == '':
+            flash('Cannot leave fields empty')
+            return render_template('new_post.html', blog_body=blog_body)
+
+        if blog_body == '':
+            flash('Cannot leave fields empty')
+            return render_template('new_post.html', blog_title=blog_title)
+
+        blog_post = Blog_post(blog_title, blog_body)
+        db.session.add(blog_post)
+        db.session.commit()
+            
     posts = Blog_post.query.all()
     return render_template('blog_list.html', posts=posts)
 
